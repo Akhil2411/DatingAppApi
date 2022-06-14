c2d4239e296ab227d9400d618d0b574c28a7d42f
Akhil.Raju <Akhil.Raju@gbg.com>
2022-06-14 13:08:29 +0530

    first commit

---
+++ DatingApp/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,34 @@
+﻿using DatingApp.Data;
+using DatingApp.Helpers;
+using DatingApp.Interfaces;
+using DatingApp.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DatingApp.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+
+            services.AddScoped<ITokenService, TokenService>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddScoped<IPhotoService, PhotoService>();
+
+            services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+
+    }
+}
